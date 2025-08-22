01aa9a860ef3b64f4bded28a6b3e8b7ec14a52bf
princethakur04 <princethakur30000@gmail.com>
2025-08-22 09:48:30 +0530

    Code for question-1

---
+++ Level1/HarrysAge.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class HarrysAge{
+	public static void(String[] args){
+		String name = "Harry";
+		int CurrYear = 2024;
+        int BirthYear = 2000;
+		int age = CurrYear - BirthYear;
+		System.out.println(name+"'s"+"age in 2024 is "+age);
+	
+}
+}
