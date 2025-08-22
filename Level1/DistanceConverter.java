7d4613173766e801ac004f0ddcd1083bb89c5e30
princethakur04 <princethakur30000@gmail.com>
2025-08-22 09:55:24 +0530

    Code for question-3

---
+++ Level1/DistanceConverter.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class DistanceConverter{
+public static void main(String[] args){
+double distance = 10.08/1.6;
+## the question is a bit incorrect , 1 mile = 1.6km !
+System.out.println("The distance 10.08 km in miles is "+distance);
+}
+}
