c353131fde718413464436c3291cf6f4227ac223
princethakur04 <princethakur30000@gmail.com>
2025-08-22 09:54:12 +0530

    Code for question-2

---
+++ Level1/SamAvg.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class SamAvg{
+public static void main(String[]args){
+String name = "Sam";
+int avg = (94+95+96)/3;
+System.out.println(name+"'s average mark in PCM is "+avg);
+
+
+}
+
+}
